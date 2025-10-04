ccf3ed73160677d508bce0689b57e86532b04b4d
RadhaGowtham <154227293+RadhaGowtham@users.noreply.github.com>
2025-10-04 17:24:18 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "jenkins" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami           = "ami-0360c520857e3138f"
+  instance_type = "t2.micro"
+  key_name      = "devops@2025"
 
   tags = {
-    Name = var.tag
+    Name = "jenkins_server"
   }
 }
