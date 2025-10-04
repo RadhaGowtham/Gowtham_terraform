fa97370574971e0002d09fd9288959d8db85d1f5
RadhaGowtham <154227293+RadhaGowtham@users.noreply.github.com>
2025-10-04 12:21:46 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,13 @@
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "terraform_state"  # Make sure this name is globally unique!
+
+  versioning {
+    enabled = true
+  }
+}
+
+}
 resource "aws_instance" "jenkins" {
   ami           = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
