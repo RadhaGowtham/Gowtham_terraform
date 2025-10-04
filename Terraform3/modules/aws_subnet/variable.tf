bf3109b28292f353f3eb1368543cce553e6fe788
RadhaGowtham <154227293+RadhaGowtham@users.noreply.github.com>
2025-10-04 17:56:14 +0530

    Create variable.tf

---
+++ Terraform3/modules/aws_subnet/variable.tf
@@ -0,0 +1,14 @@
+variable "subnet_cidr" {
+  type = string
+  default = "172.16.10.0/24"
+  
+}
+variable "subnet_name" {
+  type = string
+  default  = "tf-example"
+  } 
+}
+variable "vpc_id" {
+  type = string
+
+}
