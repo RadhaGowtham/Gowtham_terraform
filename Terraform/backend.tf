15e56dae5ddf96c35edc9c39ad36255b387ec4c3
RadhaGowtham <154227293+RadhaGowtham@users.noreply.github.com>
2025-10-04 12:32:32 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-v1"
+    bucket  = "terraform-state-rrg"
     key     = "terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
