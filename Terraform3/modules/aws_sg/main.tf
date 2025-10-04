0ad8f5e317c0e7c8f83cbb8c11068d5ae7a51567
RadhaGowtham <154227293+RadhaGowtham@users.noreply.github.com>
2025-10-04 18:03:24 +0530

    Create main.tf

---
+++ Terraform3/modules/aws_sg/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_tls" {
+   name = "allow_tls"
+   vpc_id = var.vpc_id
+   ingress {
+    description = "TLS from vpc"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+   tags = var.sg_name
+}
